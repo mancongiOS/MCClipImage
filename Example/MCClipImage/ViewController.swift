f6d5357279706c91d0ddb689950c5b1d500e8cc4
MC <13761066392@163.com>
2019-02-25 11:18:51 +0800

    initialize

---
+++ Example/MCClipImage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MCClipImage
+//
+//  Created by 562863544@qq.com on 02/25/2019.
+//  Copyright (c) 2019 562863544@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/MCClipImage/MCClipImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MCClipImage : NSObject
+@end
+@implementation PodsDummy_MCClipImage
+@end
